1c3192930596c2bc8bfcc55166dba9405a3bf01a
Oskari Rantanen <orantane@c2r2p10.hive.fi>
2020-08-21 20:49:42 +0300

    Deleted some comments from the header filer that were no longer needed

--- includes/filler.h
+++ includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: orantane <orantane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/30 17:42:36 by orantane          #+#    #+#             */
-/*   Updated: 2020/08/21 20:25:25 by orantane         ###   ########.fr       */
+/*   Updated: 2020/08/21 20:47:48 by orantane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,6 @@
 # define OFFS 10000
 
 # include "../libft/libft.h"
-# include <fcntl.h>
-
-/*
-** Megastruct to hold all the data. The "dir" array should be used to store the direction of the expansion.
-** There needs to be a calculation to create a heatmap of the opponent and an overall coverage of the map.
-** With this data, we need to create a direction to expand to. 1 for top left, 2 for top, 3 for top right
-** and so on. There should also be a small function to check the distance to the enemy, so that if there
-** is enough time the function should try to keep a small distance in order to be able to place the blocks.
-*/
 
 typedef struct	s_filler
 {
